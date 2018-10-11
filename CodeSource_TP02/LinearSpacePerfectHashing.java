d1c5394e3fb67d05787fc9b42434a8ba174de2c5
Enzeir <lehenaffpierre92@gmail.com>
2018-10-11 17:52:56 -0400

    initial commit

---
+++ CodeSource_TP02/LinearSpacePerfectHashing.java
@@ -0,0 +1,138 @@
+import java.util.Random;
+import java.util.ArrayList;
+
+public class LinearSpacePerfectHashing<AnyType>
+{
+	static int p = 46337;
+
+	QuadraticSpacePerfectHashing<AnyType>[] data;
+	int a, b;
+
+	LinearSpacePerfectHashing()
+	{
+		a=b=0; data = null;
+	}
+
+	LinearSpacePerfectHashing(ArrayList<AnyType> array)
+	{
+		AllocateMemory(array);
+	}
+
+	public void SetArray(ArrayList<AnyType> array)
+	{
+		AllocateMemory(array);
+	}
+
+	@SuppressWarnings("unchecked")
+	private void AllocateMemory(ArrayList<AnyType> array)
+	{
+		Random generator = new Random( System.nanoTime() );
+
+		if(array == null || array.size() == 0)
+		{
+			// A completer
+			return;
+		}
+		if(array.size() == 1)
+		{
+			a = b = 0;
+
+			// A completer
+			return;
+		}
+		
+		// A completer
+		a = generator.nextInt(p);
+		while(a == 0) 
+		{
+			a = generator.nextInt(p);
+		}
+		b = generator.nextInt(p);
+		
+		int m = array.size();			
+		data = new QuadraticSpacePerfectHashing[m];	
+		
+		for(int i = 0; i < array.size(); i++)
+		{
+			int key = getKey(array.get(i));
+			ArrayList<AnyType> al = new ArrayList<AnyType>();
+			
+			if(containsKey(key)) 
+			{
+				
+				for( AnyType item : data[key].items)
+				{
+					if(item != null) 
+					{
+						al.add(item);
+					}
+				}	
+			}
+			
+			al.add(array.get(i));
+			data[key] = new QuadraticSpacePerfectHashing<AnyType>(al);
+			
+		}
+	}
+
+	public int Size()
+	{
+		if( data == null ) return 0;
+
+		int size = 0;
+		for(int i=0; i<data.length; ++i)
+		{
+			size += (data[i] == null ? 1 : data[i].Size());
+		}
+		return size;
+	}
+
+	public boolean containsKey(int key)
+	{
+		// A completer
+		if(data[key] ==null)
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	public int getKey (AnyType x) {
+		// A completer
+		return ((a*x.hashCode()+b)%p)%data.length;
+	}
+	
+	public boolean containsValue (AnyType x) {
+		// A completer
+		if(data[getKey(x)] != null && data[getKey(x)].containsValue(x)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void remove (AnyType x) {
+		// A completer
+		data[getKey(x)].remove(x);
+	}
+
+	public String toString () {
+		String result = "";
+		
+		// A completer
+		for(int i = 0; i<data.length; i++) {
+			if(containsKey(i)) {
+				result += "[" + i  + "] -> " + data[i].toString() + "\n";
+
+			}
+
+		}
+		
+		return result; 
+	}
+
+	public void makeEmpty () {
+		// A completer
+		
+   	}
+	
+}
---
+++ CodeSource_TP02/QuadraticSpacePerfectHashing.java
@@ -0,0 +1,130 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class QuadraticSpacePerfectHashing<AnyType> 
+{
+	static int p = 46337;
+
+	int a, b;
+	AnyType[] items;
+
+	QuadraticSpacePerfectHashing()
+	{
+		a=b=0; items = null;
+	}
+
+	QuadraticSpacePerfectHashing(ArrayList<AnyType> array)
+	{
+		AllocateMemory(array);
+	}
+
+	public void SetArray(ArrayList<AnyType> array)
+	{
+		AllocateMemory(array);
+	}
+
+	public int Size()
+	{
+		if( items == null ) return 0;
+
+		return items.length;
+	}
+
+	public boolean containsKey(int key)
+	{
+		// A completer
+		if(items[key] == null)
+		{
+			return false;
+		}
+		return true;
+
+	}
+
+	public boolean containsValue(AnyType x )
+	{
+		// A completer
+		if(items[getKey(x)] != null && items[getKey(x)].equals(x))
+		{
+			return true;
+		}
+		return false;
+
+	}
+
+	public void remove (AnyType x) {
+		// A completer
+		items[getKey(x)] = null;
+	}
+
+	public int getKey (AnyType x) {
+		// A completer
+		int hc = x.hashCode();
+		int length = items.length;
+		int key = ((a*hc+b)%p)%length;
+		return key;
+		
+	}
+
+	@SuppressWarnings("unchecked")
+	private void AllocateMemory(ArrayList<AnyType> array)
+	{
+		Random generator = new Random( System.nanoTime() );
+
+		if(array == null || array.size() == 0)
+		{
+			// A completer
+			//rien a faire?
+			return;
+		}
+		if(array.size() == 1)
+		{
+			a = b = 0;
+			// A completer
+			items = (AnyType[]) new Object[1];
+			items[0] = array.get(0);
+			return;
+		}
+		
+		// A completer
+		a = generator.nextInt(p);
+		while(a == 0)
+			a = generator.nextInt(p);
+		
+		b = generator.nextInt(p);
+		
+		int m = array.size()*array.size();
+		items = (AnyType[]) new Object[m];
+		for(int i = 0; i < array.size(); i++)
+		{
+			int key = getKey(array.get(i));
+			
+			items[key]= array.get(i);
+			
+		}
+	}
+
+	
+	
+	public String toString () {
+		String result = "";
+		
+		for( AnyType item : items)
+		{
+			if(item != null) 
+			{
+				result += "(" + getKey(item) + ", " + item + ") ";
+			}
+		}	
+		return result; 
+	}
+
+	public void makeEmpty () {
+		   // A completer
+		 for(AnyType item : items) {
+			 remove(item);
+			 
+			 
+		 }
+   	}
+}
